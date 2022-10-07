2571e45db1eff50a681632583824b11f82906c4b
velopace <velopace@outlook.com>
2022-10-06 21:26:37 -0500

    update style with module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/Layout"
+import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Web developer based in Little Rock.</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
