5d9c1b633405b9a9920c243be8c0b4bb6b12da29
velopace <velopace@outlook.com>
2022-10-06 20:39:19 -0500

    update layout

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Navbar from './Navbar'
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <Navbar />
+      <div className="content">
+        { children }
+      </div>
+      <footer>
+        <p>Copyright 2022 VELOPACE</p>
+      </footer>
+    </div>
+  )
+}
