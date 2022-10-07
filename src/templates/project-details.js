753ca5592be3488e7ca3ed0a37536e8c6778a0ce
velopace <velopace@outlook.com>
2022-10-07 00:43:09 -0500

    generate detail page

---
+++ src/templates/project-details.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+import Layout from '../components/Layout'
+import * as styles from '../styles/project-details.module.css'
+
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+
+  return (
+    <Layout>
+      <div className={styles.details}>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div className={styles.featured}>
+          <GatsbyImage image={getImage(featuredImg)} alt="project detail" />
+        </div>
+        <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+
+`
